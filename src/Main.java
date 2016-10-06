98eec539c54c5f44fa7243c8850444c1b3618be5
owner90 <owner90@192.168.5.3>
2016-10-06 12:28:19 +0300

    2 string sabirane

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="7";
+String b = " Stupid little boys";
+System.out.println(a+b);
+
+	}
+	
+	
+
+}
